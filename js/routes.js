dcd190b3f44081489da8e98b6ac8ec5a16e9401c
wallacethefmh <wills.boyd@gmail.com>
2015-02-08 18:56:01 -0500

    injection

--- js/routes.js
+++ js/routes.js
@@ -4,6 +4,6 @@ define(function() {
   // The routes for the application. This module returns a function.
   // `match` is match method of the Router
   return function(match) {
-    match('', 'potato#show');
+    match('', 'user#show');
   };
 });
