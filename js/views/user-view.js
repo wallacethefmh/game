1bd7bb2c1c8616ac7f51f2f054c8f07d2331802d
Will <wills.boyd@gmail.com>
2015-02-08 21:39:36 -0500

    test

--- js/views/user-view.js
+++ js/views/user-view.js
@@ -18,37 +18,37 @@ define([
 
     makeCube: function() {
       
-      console.log('view:');
-      console.log(View);
-      console.log('template:');
-      console.log(template);
-      console.log('three:');
-      console.log(Three);
+      // console.log('view:');
+      // console.log(View);
+      // console.log('template:');
+      // console.log(template);
+      // console.log('three:');
+      // console.log(Three);
 
-      // var scene = new THREE.Scene();
-      // var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+      var scene = new THREE.Scene();
+      var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
 
-      // var renderer = new THREE.WebGLRenderer();
-      // renderer.setSize( window.innerWidth, window.innerHeight );
-      // document.body.appendChild( renderer.domElement );
+      var renderer = new THREE.WebGLRenderer();
+      renderer.setSize( window.innerWidth, window.innerHeight );
+      document.body.appendChild( renderer.domElement );
 
-      // var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-      // var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-      // var cube = new THREE.Mesh( geometry, material );
-      // scene.add( cube );
+      var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+      var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+      var cube = new THREE.Mesh( geometry, material );
+      scene.add( cube );
 
-      // camera.position.z = 5;
+      camera.position.z = 5;
 
-      // var render = function () {
-      //   requestAnimationFrame( render );
+      var render = function () {
+        requestAnimationFrame( render );
 
-      //   cube.rotation.x += 0.1;
-      //   cube.rotation.y += 0.1;
+        cube.rotation.x += 0.1;
+        cube.rotation.y += 0.1;
 
-      //   renderer.render(scene, camera);
-      // };
+        renderer.render(scene, camera);
+      };
 
-      // render();
+      render();
     }
   });
 
